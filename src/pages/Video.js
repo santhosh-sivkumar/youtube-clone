ccbab56798ef6f3d8a29d9f30b0eba6b13048e6f
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2024-06-05 09:29:22 +0530

    "Moved description section above channel info section in Video component"

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -153,6 +153,17 @@ const Video = () => {
         <h2 className="text-[#fff] font-semibold mt-3 mb-2 text-md lg:text-xl">
           {data?.name}
         </h2>
+        {/* Description */}
+        <div className=" bg-[#2a2a2a] my-4 rounded-[0.3rem] text-xs lg:text-sm p-3 text-[#fff]">
+          <div className="flex flex-row md:items-center">
+            <p className="font-medium pr-3 mb-2 md:mb-0">
+              {data?.views}
+              <span className="pl-1 text-xs">Views</span>
+            </p>
+            <p className="font-medium pr-3">{data?.uploadTime}</p>
+          </div>
+          <span className="text-center font-medium">{data?.description}</span>
+        </div>
         <div className="flex max-791:flex-col lg:p-2 flex-row max-791:gap-4 max-791:items-start  items-center justify-between">
           {/* channel name and subscribe button */}
           <div className="flex items-center lg:mb-0 flex-wrap">
@@ -233,17 +244,6 @@ const Video = () => {
               </div>
             </div>
           </div>
-        </div>
-        {/* Description */}
-        <div className=" bg-[#2a2a2a] mt-4 rounded-[0.3rem] text-xs lg:text-sm p-3 text-[#fff]">
-          <div className="flex flex-row md:items-center">
-            <p className="font-medium pr-3 mb-2 md:mb-0">
-              {data?.views}
-              <span className="pl-1 text-xs">Views</span>
-            </p>
-            <p className="font-medium pr-3">{data?.uploadTime}</p>
-          </div>
-          <span className="text-center font-medium">{data?.description}</span>
         </div>
 
         {/* Comments */}
